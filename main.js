fc008f87909cf539a6b930d803c21d54ebc3d3f1
rjyoung11 <ryoung@netlinkvoice.com>
2022-10-15 14:57:36 +0800

    Added functionalities in accepting and denying requests

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var listOfRequests = [];
+var selectedItem = "";
 function bySelectedRequest(id, value) {
   $("#list").empty();
   document.getElementById("header-title").innerHTML = value + " Request/s";
@@ -7,10 +9,16 @@ function bySelectedRequest(id, value) {
     url: "https://reqres.in/api/users",
     data: "",
     success: function (data) {
+      listOfRequests = data.data;
       for (let index = 0; index < data.data.length; index++) {
         var ul = document.getElementById("list");
         var li = document.createElement("li");
-        li.appendChild(document.createTextNode(data.data[index].first_name + " " + data.data[index].last_name));
+        li.appendChild(
+          document.createTextNode(
+            data.data[index].first_name + " " + data.data[index].last_name
+          )
+        );
+        li.setAttribute("id", data.data[index].id);
         ul.appendChild(li);
       }
     },
@@ -25,3 +33,35 @@ function getNumbersOfRequest(id) {
     x.style.display = "none";
   }
 }
+
+$("#list").on("click", function (e) {
+  selectedItem = listOfRequests.filter((data) => {
+    if (data.id === Number($(e.target).attr("id"))) return data;
+  });
+  if (confirm("You want to accept request of " + selectedItem[0].first_name + " " + selectedItem[0].last_name + "!")) {
+    alert("Request of " + selectedItem[0].first_name + " " + selectedItem[0].last_name + " is accepted!");
+  } else {
+    alert("Request of " + selectedItem[0].first_name + " " + selectedItem[0].last_name + " is denied!");
+  }
+
+  listOfRequests.splice(listOfRequests.indexOf(selectedItem[0]), 1);
+  $("#list").empty();
+  for (let index = 0; index < listOfRequests.length; index++) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(
+        listOfRequests[index].first_name + " " + listOfRequests[index].last_name
+      )
+    );
+    li.setAttribute("id", listOfRequests[index].id);
+    ul.appendChild(li);
+  }
+
+  console.log(listOfRequests);
+
+  // for (let index = 0; index < listOfRequests.length; index++) {
+  //   selectedItem = listOfRequests[index].id === $(e.target).attr('id')
+  // }
+  // console.log()
+});
