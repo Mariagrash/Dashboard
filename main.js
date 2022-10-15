e330b762cc567bef3282c70bd6c5e6dc269611b3
rjyoung11 <ryoung@netlinkvoice.com>
2022-10-15 14:30:53 +0800

    Added function in show modals and number of request

--- main.js
+++ main.js
@@ -7,17 +7,21 @@ function bySelectedRequest(id, value) {
     url: "https://reqres.in/api/users",
     data: "",
     success: function (data) {
-      document.getElementById(id).innerHTML = data.data.length;
       for (let index = 0; index < data.data.length; index++) {
         var ul = document.getElementById("list");
         var li = document.createElement("li");
-        li.appendChild(
-          document.createTextNode(
-            data.data[index].first_name + " " + data.data[index].last_name
-          )
-        );
+        li.appendChild(document.createTextNode(data.data[index].first_name + " " + data.data[index].last_name));
         ul.appendChild(li);
       }
     },
   });
+}
+function getNumbersOfRequest(id) {
+  var x = document.getElementById(id);
+  if (!x.style.display || x.style.display === "none") {
+    x.style.display = "block";
+    x.innerHTML = 6;
+  } else {
+    x.style.display = "none";
+  }
 }
